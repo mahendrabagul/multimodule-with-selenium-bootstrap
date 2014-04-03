e088fa81d5ae91320445b818437c02745f0d028a
Mahendra Bagul <bagulm123@gmail.com>
2014-04-03 11:19:14 +0000

    web.sql edited online with Bitbucket

--- common/src/db/web.sql
+++ common/src/db/web.sql
@@ -1,6 +1,33 @@
-CREATE TABLE roles ( id int(6) NOT NULL AUTO_INCREMENT, role varchar(20) NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-CREATE TABLE users ( id int(6) NOT NULL AUTO_INCREMENT, login varchar(20) NOT NULL, password varchar(20) NOT NULL, PRIMARY KEY (id) ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-CREATE TABLE user_roles ( user_id int(6) NOT NULL, role_id int(6) NOT NULL, KEY user (user_id), KEY role (role_id) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `user_roles`;
+
+CREATE TABLE `user_roles` (
+  `user_id` int(6) NOT NULL,
+  `role_id` int(6) NOT NULL,
+  KEY `user` (`user_id`),
+  KEY `role` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `emailid` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `mobileNo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`,`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO neova.roles (role) VALUES ('admin'), ('moderator');
 INSERT INTO neova.users (login, password) VALUES ('user', 'user'), ('admin', 'admin');
